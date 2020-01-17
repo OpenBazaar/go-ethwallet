2f561017d34caa6f455b84d6b6013e7e1006cdfd
Ashwin Mangale <ashwin.mangale@gmail.com>
2020-01-17 22:15:48 +0530

    fixes

--- wallet/client.go
+++ wallet/client.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"math/big"
 	"net/http"
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -787,8 +787,9 @@ func (wallet *EthereumWallet) AssociateTransactionWithOrder(txnCB wi.Transaction
 	}
 }
 
-// checkTxnRcpt check the txn rcpt status
-func (wallet *EthereumWallet) checkTxnRcpt(hash *common.Hash, data []byte) (*common.Hash, error) {
+// CheckTxnRcpt check the txn rcpt status
+func (wallet *EthereumWallet) CheckTxnRcpt(hash *common.Hash, data []byte) (*common.Hash, error) {
+
 	var rcpt *types.Receipt
 	pTxn, err := DeserializePendingTxn(data)
 	if err != nil {
@@ -1428,8 +1429,8 @@ func (wallet *EthereumWallet) Multisign(ins []wi.TransactionInput, outs []wi.Tra
 	return tx.Hash().Bytes(), nil
 }
 
-// AddWatchedAddress - Add a script to the wallet and get notifications back when coins are received or spent from it
-func (wallet *EthereumWallet) AddWatchedAddress(addrs btcutil.Address) error {
+// AddWatchedAddresses - Add a script to the wallet and get notifications back when coins are received or spent from it
+func (wallet *EthereumWallet) AddWatchedAddresses(addrs ...btcutil.Address) error {
 	// the reason eth wallet cannot use this as of now is because only the address
 	// is insufficient, the redeemScript is also required
 	return nil
